09ccf637ee7914607cb01c5ded65bd6cdc63d4ea
ozz <105267847+Overfit-Master@users.noreply.github.com>
2025-04-18 21:01:54 +0800

    Update model.py

--- Retrival/model.py
+++ Retrival/model.py
@@ -2,12 +2,14 @@ import warnings
 import torchvision
 import torch.nn as nn
 
+# pretrained的用法是老版本，所以会报警告，此处忽略处理，可以使用weight进行加载
 warnings.simplefilter("ignore", UserWarning)
 flower_model = torchvision.models.resnet18(pretrained=True)
 car_model = torchvision.models.resnet18(pretrained=True)
 classify_model = torchvision.models.resnet18(pretrained=True)
 warnings.resetwarnings()
 
+# 花的类别为17，车的类别为196， 2为车花二分类模型的类别数
 flower_classes = 17
 car_classes = 196
 classify_classes = 2
